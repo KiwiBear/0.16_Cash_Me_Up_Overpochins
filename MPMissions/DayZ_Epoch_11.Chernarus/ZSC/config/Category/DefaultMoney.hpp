aed070a74acb670a1d52c4b5257b3508043efb22
fallingsheep <razorsoftinfo@gmail.com>
2015-06-02 15:34:15 +1000

    overpoch first commit

---
+++ MPMissions/DayZ_Epoch_11.Chernarus/ZSC/config/Category/DefaultMoney.hpp
@@ -0,0 +1,57 @@
+class Category_700 {
+	class ItemBriefcase100oz {
+		type = "trade_items";
+		buy[] ={110000,"Coins"};
+		sell[] ={110000,"Coins"};
+	};
+	class ItemGoldBar {
+		type = "trade_items";
+		buy[] ={1000,"Coins"};
+		sell[] ={1000,"Coins"};
+	};
+	class ItemGoldBar10oz {
+		type = "trade_items";
+		buy[] ={11000,"Coins"};
+		sell[] ={11000,"Coins"};
+	};
+	class ItemSilverBar {
+		type = "trade_items";
+		buy[] ={10,"Coins"};
+		sell[] ={10,"Coins"};
+	};
+	class ItemSilverBar10oz {
+		type = "trade_items";
+		buy[] ={100,"Coins"};
+		sell[] ={100,"Coins"};
+	};
+	class ItemCopperBar10oz {
+		type = "trade_items";
+		buy[] ={5000,"Coins"};
+		sell[] ={5000,"Coins"};
+	};
+	class ItemTinBar {
+		type = "trade_items";
+		buy[] ={2,"Coins"};
+		sell[] ={2,"Coins"};
+	};
+    class ItemAluminumBar {
+	    type = "trade_items";                 
+		buy[] ={2,"Coins"};
+		sell[] ={2,"Coins"};
+	};
+    class PartOre {
+	    type = "trade_items";                 
+		buy[] ={20,"Coins"};
+		sell[] ={20,"Coins"};
+    };
+	class PartOreSilver {
+	    type = "trade_items";                 
+		buy[] ={30,"Coins"};
+		sell[] ={30,"Coins"};
+    };
+	class PartOreGold {
+	    type = "trade_items";                 
+		buy[] ={1000,"Coins"};
+		sell[] ={1000,"Coins"};
+    };
+};
---
+++ MPMissions/DayZ_Epoch_11.Chernarus/ZSC/config/Category/NeutralPackagedFood.hpp
@@ -0,0 +1,94 @@
+class Category_687 {
+	class FoodCanBakedBeans {
+		type = "trade_items";
+		buy[] ={60,"Coins"};
+		sell[] ={30,"Coins"};
+	};
+	class FoodCanPasta {
+		type = "trade_items";
+		buy[] ={60,"Coins"};
+		sell[] ={30,"Coins"};
+	};
+	class FoodCanFraggleos {
+		type = "trade_items";
+		buy[] ={60,"Coins"};
+		sell[] ={30,"Coins"};
+    };
+	class FoodCanGriff {
+		type = "trade_items";
+		buy[] ={60,"Coins"};
+		sell[] ={30,"Coins"};
+	 };
+	class FoodCanCurgon {
+		type = "trade_items";
+		buy[] ={60,"Coins"};
+		sell[] ={30,"Coins"};
+	};
+	class FoodCanSardines {
+		type = "trade_items";
+		buy[] ={60,"Coins"};
+		sell[] ={30,"Coins"};
+	};
+	class FoodMRE {
+		type = "trade_items";
+		buy[] ={200,"Coins"};
+		sell[] ={100,"Coins"};
+	};
+	class FoodPistachio {
+		type = "trade_items";
+		buy[] ={7000,"Coins"};
+		sell[] ={7000,"Coins"};
+	};
+	class FoodNutmix {
+		type = "trade_items";
+		buy[] ={7000,"Coins"};
+		sell[] ={7000,"Coins"};
+	};
+	class FoodCanUnlabeled {
+		type = "trade_items";
+		buy[] ={30,"Coins"};
+		sell[] ={30,"Coins"};
+	};
+};
+class Category_579 {
+	class FoodCanBakedBeans {
+		type = "trade_items";
+		buy[] ={60,"Coins"};
+		sell[] ={30,"Coins"};
+	};
+	class FoodCanFrankBeans {
+		type = "trade_items";
+		buy[] ={60,"Coins"};
+		sell[] ={30,"Coins"};
+	};
+	class FoodCanPasta {
+		type = "trade_items";
+		buy[] ={60,"Coins"};
+		sell[] ={30,"Coins"};
+	};
+	class FoodCanSardines {
+		type = "trade_items";
+		buy[] ={60,"Coins"};
+		sell[] ={30,"Coins"};
+	};
+	class FoodMRE {
+		type = "trade_items";
+		buy[] ={200,"Coins"};
+		sell[] ={100,"Coins"};
+	};
+	class FoodPistachio {
+		type = "trade_items";
+		buy[] ={7000,"Coins"};
+		sell[] ={7000,"Coins"};
+	};
+	class FoodNutmix {
+		type = "trade_items";
+		buy[] ={7000,"Coins"};
+		sell[] ={7000,"Coins"};
+	};
+	class FoodCanUnlabeled {
+		type = "trade_items";
+		buy[] ={60,"Coins"};
+		sell[] ={30,"Coins"};
+	};
+};
